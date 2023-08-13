35053db8e0fffa2220d3b0e9fd16d1982c683215
avbalsam <avbalsam@gmail.com>
2023-08-13 19:24:57 +0300

    Change MNIST loading

--- Code/mnist_loader.py
+++ Code/mnist_loader.py
@@ -6,6 +6,8 @@ A library to load the MNIST image data.  For details of the data
 structures that are returned, see the doc strings for ``load_data``
 and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
 function usually called by our neural network code.
+
+Credit: Nielsen, 2015
 """
 
 #### Libraries
